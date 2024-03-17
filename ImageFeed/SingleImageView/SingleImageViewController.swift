de453f37397f77eb566026c42218aeb57f78fd28
glomebom <gleb.khodosevich@gmail.com>
2024-03-17 12:10:44 +0300

    Спринт 9/20: 9 → Тема 2/6: Навигация → Урок 4

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Gleb on 17.03.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
