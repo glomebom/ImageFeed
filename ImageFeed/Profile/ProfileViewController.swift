18ccf7628e8d2bc40692a1b0f7a4af5ae4af1dd3
glomebom <gleb.khodosevich@gmail.com>
2024-03-29 11:42:06 +0300

    Сдаём задачу спринта 9 на ревью

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -17,6 +17,73 @@ final class ProfileViewController: UIViewController {
     
     @IBOutlet weak var logoutButton: UIButton!
     
+    override func viewDidLoad() {
+        view.backgroundColor = .ypBlack
+        
+        // Создание фото профиля
+        let profileImage = UIImage(named: "Photo")
+        let imageView = UIImageView(image: profileImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        // Констрейнты для фото профиля
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        
+        // Создание кнопки выхода
+        let exitButton = UIButton()
+        let exitImage = UIImage(named: "exit")
+        exitButton.setImage(exitImage, for: .normal)
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+
+        // Констрейнты для кнопки выхода
+        exitButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        exitButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        
+        
+        // Создание лейбла с именем
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: UIFont.Weight(rawValue: 700.00))
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        // Констрейнты для лейбла с именем
+        nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        
+        
+        //Создание лейбла с ником
+        let nickNameLabel = UILabel()
+        nickNameLabel.text = "@ekaterina_nov"
+        nickNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .light)
+        nickNameLabel.textColor = .init(red: 174, green: 175, blue: 180, alpha: 1)
+        nickNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nickNameLabel)
+        
+        // Констрейнты для лейбла с ником
+        nickNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        nickNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        // Создание лейбла с описанием
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .light)
+        descriptionLabel.textColor = .ypWhite
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        // Констрейнты для лейбла с описанием
+        descriptionLabel.leadingAnchor.constraint(equalTo: nickNameLabel.leadingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: nickNameLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
     @IBAction func didTapLogoutButton() {
     }
 }
