a0d241a8a13611618ab062183d2b593e5acb87f9
glomebom <gleb.khodosevich@gmail.com>
2024-04-01 23:14:25 +0300

    Спринт 10/20: 10 → Тема 2/8: Работа с Unsplash API → Урок 3/3

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Gleb on 01.04.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "hY27nd2PJ9ar1uZBdeKkvMuLkzIffVG33Ii7xISv4so"
+    static let secretKey = "KIDM1uvh63I6HD2s13kBdiuCH3_04SqBC2Ct3zsKkD0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
