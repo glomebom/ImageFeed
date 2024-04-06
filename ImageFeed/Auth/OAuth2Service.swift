f4d32b026a71f3da03f310543a16ebb075bf99ce
glomebom <gleb.khodosevich@gmail.com>
2024-04-06 19:52:39 +0300

    Спринт 10/20: 10 → Тема 6/8: Делаем сетевой запрос → Урок 3/3 makeOAuthTokenRequest

--- ImageFeed/Auth/OAuth2Service.swift
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -11,5 +11,22 @@ final class OAuth2Service {
     static let shared = OAuth2Service()
     //private init()
     
+    func makeOAuthTokenRequest(code: String) -> URLRequest {
+        let baseURL = URL(string: "https://unsplash.com")!
+        if let url = URL(
+            string: "/oauth/token"
+            + "?client_id=\(Constants.accessKey)"         // Используем знак ?, чтобы начать перечисление параметров запроса
+            + "&&client_secret=\(Constants.secretKey)"    // Используем &&, чтобы добавить дополнительные параметры
+            + "&&redirect_uri=\(Constants.redirectURI)"
+            + "&&code=\(code)"
+            + "&&grant_type="authorization_code",
+            relativeTo: baseURL                           // Опираемся на основной или базовый URL, которые содержат схему и имя хоста
+        ) {
+            if var request = URLRequest(url: url) {
+                request.httpMethod = "POST"
+                return request
+            }
+        }
+    }
 }
 
